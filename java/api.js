7845969e6d67483cac94fa38b5458324ecb6664c
joselechuga <jos.medinav@duocuc.cl>
2022-05-14 06:24:51 -0400

    api rest

---
+++ java/api.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#enviar").click(function(){
+      
+        $.getJSON('https://mindicador.cl/api', function(data) {
+    var dailyIndicators = data;
+    $("<p/>", {
+        html: 'El valor actual del RP es $' + dailyIndicators.uf.valor
+    }).appendTo("#precio");
+}).fail(function() {
+    console.log('Error al consumir la API!');
+});
+
+
+    });
+});
+
